4ad8b6a5e525c88ad4c008cd04c73e3d1ed37c11
Hen0k <henoktilaye7@gmail.com>
2022-06-16 16:52:35 +0300

    ui: setup landing basic landing page with nav bar

--- frontend/src/components/landingPage/headerBar.js
+++ frontend/src/components/landingPage/headerBar.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './headerBar.css'
+import logo from './logo.png'
+
+
+// const connectWalletHandler = () => {
+
+// }
+
 
 const Header = () => {
+
+
     return (
-        <header className="App-header" > 
-            <ul>
-            <li>tenx dashboard</li>
-            <li>about</li>
-            <li>connect wallet</li>
-            </ul>
+        <header class="App-header">
+            <div class="wrapper site-header__wrapper">
+                <a href="/" class="brand"><img src={logo} alt="brand" /></a>
+                <nav class="nav">
+                    <a href="/">home</a>
+                    <a target="_blank" rel="noreferrer noopener" href="https://tenx.10academy.org/" >tenx dashboard</a>
+                    <a href="/" >connect wallet</a>
+                </nav>
+                
+            </div>
         </header>
     )
 }
