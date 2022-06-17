a3b2a62a52bbcf3d77efad232a9296ef6886c26c
Hen0k <henoktilaye7@gmail.com>
2022-06-17 16:35:22 +0300

    backend: Add routest for login, logout, and a dummy jwt protected route

--- backend/flask_app/__init__.py
+++ backend/flask_app/__init__.py
@@ -1,23 +1,26 @@
 """Initialize Flask app."""
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from flask_jwt_extended import JWTManager
 from flask_app.routes import init_routes
 
 
 db = MongoEngine()
+jwt = JWTManager()
 
 
 def init_app():
     """Construct core Flask application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object('config.ApplicationConfig')
 
     # Initialize Plugins
     db.init_app(app)
-    
+    jwt.init_app(app)
+
+    # Initialize routes
     app = init_routes(app)
-    
 
     # with app.app_context():
-        # from . import routes
+
     return app
