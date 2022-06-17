8a4a0cad240def18bc4025630239bb6ca64d791a
Hen0k <henoktilaye7@gmail.com>
2022-06-17 20:26:49 +0300

    frontend: add bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React from 'react';
-import PopupPermissions from './components/landingPage/PopupPermissions'
+// import PopupPermissions from './components/landingPage/PopupPermissions'
 import Header from './components/landingPage/headerBar';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <h1>Welcome to 10 Academy's NFT certificates minter and distributor</h1>
-      <PopupPermissions />
+      <h3>Build Sign Up & Login UI Template in React</h3>
+      {/* <Header /> */}
+      {/* <h1>Welcome to 10 Academy's NFT certificates minter and distributor</h1> */}
+      {/* <PopupPermissions /> */}
     </div>
   );
 }
