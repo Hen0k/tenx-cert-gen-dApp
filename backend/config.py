ec3b56f373ddbd5799bd4d433e3bb4b6bb0c10b1
Hen0k <henoktilaye7@gmail.com>
2022-06-17 16:29:37 +0300

    backend: Add JWT configurations

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,18 @@
 import os
+from uuid import uuid4
 from dotenv import load_dotenv
+from datetime import timedelta
 
 
 load_dotenv()
 
+def get_uuid():
+    return uuid4().hex
 
-class Config(object):
+class ApplicationConfig(object):
     """ Flask application config """
+    JWT_SECRET_KEY = get_uuid()
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     # SECRET_KEY = os.urandom(20).hex()
 
     # Flask-MongoEngine settings
