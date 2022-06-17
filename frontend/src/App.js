1a2c03250942191aa811f5f535837efd82bca895
Hen0k <henoktilaye7@gmail.com>
2022-06-17 21:10:44 +0300

    frontend: move bootstrap to index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import logo from './logo.png'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
